38a2cc386348e76af3c62c6b7aa1666b5da41994
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2021-03-01 17:01:34 +0530

    Update Person.kt

--- CRUD Operation at Server/Person.kt
+++ CRUD Operation at Server/Person.kt
@@ -9,3 +9,18 @@ data class Person(
     var email : String,
     var mobile : Long
 )
+
+@Serializable
+data class FailResponse(
+    var status : Int, 
+    var message : String, 
+    var isSuccess : Boolean
+)
+
+@Serializable
+data class SuccessResponse(
+    var status: Int, 
+    var message: String, 
+    var isSuccess: Boolean,
+    var person: Person
+)
