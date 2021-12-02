6c3c4b51fd1a860593dfcfa2491de5710a54cfa4
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2021-12-02 10:13:54 +0530

    Insert, Read, Update, Delete

--- Insert_read_update.kt
+++ Insert_read_update.kt
@@ -45,30 +45,40 @@ fun getDatabase() : Database? {
     }else database
     return database
 }
+
+
 fun Routing.dataRouting(){
 
     val personsList = arrayListOf<Person>()
-    
+
     post("/add"){
         val person = call.receive<Person>()
-        getDatabase()?.insert(PersonTable){
+        val affectedRows = getDatabase()?.insert(PersonTable){
             set(it.name,person.name)
             set(it.email,person.email)
             set(it.mobile,person.mobile)
         }
-        call.respond(CommonResponse(statusCode = 200, message = "Person ${person.name} Added in Database",null))
+        if(affectedRows == 1){
+            call.respond(CommonResponse(statusCode = HttpStatusCode.OK.value, message = "Person ${person.name} Added in Database",null))
+        }else{
+            call.respond(CommonResponse(statusCode = HttpStatusCode.NotFound.value, message = "Error to Add data in Database",null))
+        }
     }
-    
-      get("/view"){
-        personsList.clear()
+
+    get("/view"){
         //Following code like > SELECT * FROM PersonTable;
+        personsList.clear()
         val query: Query? = getDatabase()?.from(PersonTable)?.select()
         for (row in query!!){
             personsList.add(Person(row[PersonTable.id]!!,row[PersonTable.name]!!,row[PersonTable.email]!!,row[PersonTable.mobile]!!))
         }
-        call.respond(CommonResponse(statusCode = 200, message = "${personsList.size} Person(s) Found",personsList))
+        if(personsList.isNotEmpty()){
+            call.respond(CommonResponse(statusCode = HttpStatusCode.OK.value, message = "${personsList.size} Person(s) Found",personsList))
+        }else{
+            call.respond(CommonResponse(statusCode = HttpStatusCode.NotFound.value, message = "No Person(s) Found",null))
+        }
     }
-      
+
     get("/view/{id}"){
         personsList.clear()
         val id : Int = call.parameters["id"]?.toInt() ?: return@get call.respond(CommonResponse(statusCode = HttpStatusCode.BadRequest.value,"Invalid parameter",null))
@@ -89,8 +99,7 @@ fun Routing.dataRouting(){
         }
     }
 
-      
-   post("/update"){
+    post("/update"){
         val person = call.receive<Person>()
         val affectedRows = getDatabase()?.update(table = PersonTable){
             set(it.name,person.name)
@@ -101,27 +110,33 @@ fun Routing.dataRouting(){
             }
         }
         if(affectedRows == 1){  // 1 Row affected i.e. Data Updated  because we apply where clause to update single row
-            call.respond(CommonResponse(statusCode = 200, message = "${person.name} Updated",null))
+            call.respond(CommonResponse(statusCode = HttpStatusCode.OK.value, message = "${person.name} Updated",null))
         }else {
             call.respond(CommonResponse(statusCode = HttpStatusCode.NotFound.value, message = "Data Could not Updated", null))
         }
     }
-    
-    post("/delete"){    //----------------------------------> Take Complete Object and Delete row in MySQL
+
+    post("/delete"){
         val person = call.receive<Person>()
-        getDatabase()?.delete(table = PersonTable){
+        val affectedRows = getDatabase()?.delete(table = PersonTable){
             it.id eq person.id
         }
-        call.respond(CommonResponse(statusCode = 200, message = "${person.name} Deleted",null))
+        if(affectedRows == 1){
+            call.respond(CommonResponse(statusCode = HttpStatusCode.OK.value, message = "${person.name} Deleted From Database",null))
+        }else{
+            call.respond(CommonResponse(statusCode = HttpStatusCode.NotFound.value, message = "Could not delete",null))
+        }
     }
 
-    post("/delete/{id}"){ //----------------------------------> Take id only and Delete row in MySQL
+    post("/delete/{id}"){
         val id = call.parameters["id"].toString().toInt()
-        getDatabase()?.delete(table = PersonTable){
+        val affectedRows = getDatabase()?.delete(table = PersonTable){
             it.id eq id
         }
-        call.respond(CommonResponse(statusCode = 200, message = "Id $id deleted",null))
+        if(affectedRows == 1){
+            call.respond(CommonResponse(statusCode = HttpStatusCode.OK.value, message = "Id $id deleted",null))
+        }else{
+            call.respond(CommonResponse(statusCode = HttpStatusCode.NotFound.value, message = "Id $id not available in database",null))
+        }
     }
-    
-    
 }
